6c9955245fde6170f8f9ff28ed0828eb211979bd
Net-zen <prankster@myttk.ru>
2020-08-21 23:57:18 +0600

    fix: hidding bird name before answer

--- src/components/question/question.js
+++ src/components/question/question.js
@@ -39,7 +39,7 @@ const Question = ({ bird, answer, isAnswerRight }) => {
       <img src={isAnswerRight ? bird.image : hiddenBirdImage} alt="bird" className="bird-image" />
       <div className="question-data">
         <ul className="list-group list-group-flush">
-          <li className="list-group-item"><h3>{isAnswerRight ? bird.name : bird.name}</h3></li>
+          <li className="list-group-item"><h3>{isAnswerRight ? bird.name : '*****'}</h3></li>
           <li className="list-group-item">
             <AudioPlayer
               src={bird.audio}
